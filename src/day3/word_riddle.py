de5df55d63901e4b7444e5c8b93b1ca71fbf270e
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-12-04 10:49:56 +0100

    feat: WordRiddle has correct height

--- src/day3/word_riddle.py
+++ src/day3/word_riddle.py
@@ -2,6 +2,7 @@
 class WordRiddle:
     content: str
     width: int
+    height: int
 
     def __init__(self, init_str):
         self.content = init_str.replace('\n', '')
@@ -10,9 +11,11 @@ class WordRiddle:
             self.width = found
         else:
             self.width = len(init_str)
-
+        self.height = len(self.content) / self.width
 
     def get_width(self) -> int:
         return self.width
-
+    def get_height(self) -> int:
+        return self.height
+    
 
--- tests/test_day3.py
+++ tests/test_day3.py
@@ -10,3 +10,14 @@ from day3.word_riddle import WordRiddle
 def test_get_width(init_str,expected_width) -> None:
     testee = WordRiddle(init_str)
     assert testee.get_width() == expected_width
+
+@pytest.mark.parametrize('init_str, expected_height', [
+    ('X', 1),
+    ('XM\nAS', 2),
+    ('XMA\nAXM', 2),
+])
+def test_get_width(init_str,expected_height) -> None:
+    testee = WordRiddle(init_str)
+    assert testee.get_height() == expected_height
+
+
