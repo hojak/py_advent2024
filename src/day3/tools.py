1054d42d3546a9b97c877c1e2fb10ed91905df0f
Holger Janßen-Kroll <github@ho-jk.de>
2024-12-04 02:45:18 +0100

    feat: compute result of a program

--- src/day3/tools.py
+++ src/day3/tools.py
@@ -1,12 +1,20 @@
 import re
+from functools import reduce
 
 def find_mul_statements ( input: str ) -> list :
     return re.findall ( r'(mul\(\d{1,3},\d{1,3}\))', input)
 
 def get_mul_result ( input:str )-> int :
     m = re.search(r'mul\((\d+),(\d+)\)', input)
-    
+
     if m:
         return int(m.group(1)) * int(m.group(2))
     
     return 0
+
+def get_program_result ( input: str) -> int:
+    mul_statements = find_mul_statements(input)
+    
+    results = list(map(get_mul_result, mul_statements))
+
+    return reduce ( lambda a,b : a+b, results)
--- tests/test_day3.py
+++ tests/test_day3.py
@@ -1,4 +1,4 @@
-from day3.tools import find_mul_statements, get_mul_result
+from day3.tools import find_mul_statements, get_mul_result, get_program_result
 
 import pytest
 
@@ -21,5 +21,6 @@ def test_find_mul_statements(input: str, expectedFindings:list) -> None:
 def test_get_mul_result(input: str, expectedValue:int) -> None:
     assert get_mul_result(input) == expectedValue
 
-
+def test_get_program_result() -> None:
+    assert get_program_result ('xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))') == 161
 
