211e7ee8294f906a37a285df4783a9bae060d419
Holger Janßen-Kroll <github@ho-jk.de>
2024-12-04 01:35:52 +0100

    feat: is_save works

--- src/day2/report_checks.py
+++ src/day2/report_checks.py
@@ -1,2 +1,15 @@
-def is_save ( list_of_leves: list)->bool:
+def is_save ( list_of_leves: list) -> bool:
+    if len(list_of_leves)<2:
+        return True
+    
+    first_diff = list_of_leves[1] - list_of_leves[0]
+
+    for index in range (len(list_of_leves)-1):
+        diff = list_of_leves[index+1]-list_of_leves[index]
+
+        if ( diff * first_diff < 0 ):
+            return False
+        if ( abs(diff)<1 or abs(diff)>3):
+            return False
+
     return True
--- tests/test_day2.py
+++ tests/test_day2.py
@@ -4,7 +4,16 @@ import pytest
 
 @pytest.mark.parametrize('list_of_levels, expected', [
     ([], True),
+    ([1], True),
+    ([1,3], True),
+    ([1,3,2], False),
+    ([3,2,1], True),
+    ([3,1,2], False),
+    ([1,4,7], True),
+    ([1,5,7], False),
+    ([1,2,2], False),
+    ([10,8,5], True),
+    ([10,8,4], False),
 ])
-
 def test_is_save(list_of_levels: list, expected:bool) -> None:
     assert is_save(list_of_levels) == expected
