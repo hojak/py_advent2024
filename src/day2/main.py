428932df1cdc6bae2c7da872e428923384a02a82
Holger Janßen-Kroll <github@ho-jk.de>
2024-12-04 02:22:58 +0100

    fix: dampener completely working
    sample input was not sufficient

--- src/day2/main.py
+++ src/day2/main.py
@@ -1,6 +1,6 @@
 import sys
 
-from day2.report_checks import count_save_reports
+from day2.report_checks import count_save_reports, count_save_reports_with_dampener
 from adv24_tools.tools import read_from_file, split_lines_into_numbers
 
 def main(filename) -> None:
@@ -9,6 +9,10 @@ def main(filename) -> None:
 
     print ("Number of Save Reports:")
     print (count_save_reports(list_of_reports))
+
+    print ("\nNumber of Save Reports with Dampener:")
+    print (count_save_reports_with_dampener(list_of_reports))
+
 
 if __name__ == "__main__":
     main(sys.argv[1])
--- src/day2/report_checks.py
+++ src/day2/report_checks.py
@@ -16,7 +16,11 @@ def is_current_diff_save ( first_diff: int, current_diff: int) -> bool:
 
 def count_save_reports ( list_of_reports: list) -> int:
     save_reports = list(filter(is_save, list_of_reports))
-    return len ( save_reports)
+    return len (save_reports)
+
+def count_save_reports_with_dampener ( list_of_reports: list) -> int:
+    save_reports = list(filter(is_save_with_dampener, list_of_reports))
+    return len (save_reports)
 
 def is_save_with_dampener (list_of_levels: list) -> bool:
     if len(list_of_levels)<2:
@@ -25,7 +29,11 @@ def is_save_with_dampener (list_of_levels: list) -> bool:
     first_diff = list_of_levels[1] - list_of_levels[0]
 
     for index in range (len(list_of_levels)-1):
-        if ( not is_current_diff_save ( first_diff, list_of_levels[index+1]-list_of_levels[index])):
-            return is_save ( list_of_levels[:index] + list_of_levels[index+1:])
+        if ( not is_current_diff_save ( first_diff, list_of_levels[index+1]-list_of_levels[index])):            
+            # either current element is the problem, or the next one, or the first element gives a wrong "direction"
+            result = is_save ( list_of_levels[:index] + list_of_levels[index+1:]) \
+                or is_save ( list_of_levels[:index+1] + list_of_levels[index+2:]) \
+                or is_save ( list_of_levels[1:])
+            return result
 
     return True
