df53ba356d82b426c74f3296c005d398a2cc9c38
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-12-05 12:21:07 +0100

    feat: implement solver for day5 part 1

--- src/day5/Solver.py
+++ src/day5/Solver.py
@@ -1,12 +1,14 @@
 from adv24_tools.problemSolver import problemSolver
+from day5.ReleaseRequirements import ReleaseRequirements
 
 class Solver(problemSolver):
 
     def parse_input(self):
-        return super().parse_input()
+        return ReleaseRequirements(self.input)
 
     def solve_part1(self):
-        return super().solve_part1()
+        print ("sum of pagenumbers for the middle page in valid updates:")
+        print (self.parsed_input.get_sum_for_valid_updates())
     
     def solve_part2(self):
         return super().solve_part2()
