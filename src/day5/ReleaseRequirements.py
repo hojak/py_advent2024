96ddae653b0e6a04f1810c8c85ec482931e9997d
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-12-05 11:18:58 +0100

    feat: analyze rules

--- src/day5/ReleaseRequirements.py
+++ src/day5/ReleaseRequirements.py
@@ -2,11 +2,22 @@ import re
 
 class ReleaseRequirements:
     input: str
+    requirements: dict
 
     def __init__(self,input: str):
         (rules_input, updates_input) = re.split (r'\n\s*\n', input)
-        self.rules = re.split( r'\n', rules_input)
+        self.rules = self.init_rules ( rules_input )
+        
         self.updates = re.split ( r'\n', updates_input)
+
+    def init_rules ( self, rules_input): 
+        self.requirements = {}
+
+        for rule in re.split( r'\n', rules_input):
+            (required_page, entry_page) = rule.split('|',2)
+            if ( not int(entry_page) in self.requirements ):
+                self.requirements[int(entry_page)] = []
+            self.requirements[int(entry_page)].append ( int(required_page) )
 
 
     def get_rules(self) -> list:
--- tests/test_day5.py
+++ tests/test_day5.py
@@ -7,10 +7,23 @@ from day5.ReleaseRequirements import ReleaseRequirements
 #     ('XM\nAS', 2),
 #     ('XMA\nAXM', 3),
 # ])
-def test_requirements_init() -> None:
+def test_requirements_init_updates() -> None:
     testee = ReleaseRequirements('''10|12
                                  
 11,12
 12,7,10''')
-    assert len(testee.get_rules()) == 1
     assert len(testee.get_updates()) == 2
+
+@pytest.mark.parametrize('init_str, expected_requirements', [
+    ('''10|12
+                                 
+11,12''', {12: [10]}),
+    ('''11|12
+10|12
+10|13
+                                 
+11,12''', {12: [11,10], 13: [10]}),
+])
+def test_requirements_init_updates(init_str, expected_requirements) -> None:
+    testee = ReleaseRequirements(init_str)
+    assert testee.requirements == expected_requirements
