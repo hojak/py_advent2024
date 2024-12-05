0cff1bda8b519ea6d3c85508101ff88db1968e14
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-12-05 11:26:48 +0100

    feat: parse updates

--- src/day5/ReleaseRequirements.py
+++ src/day5/ReleaseRequirements.py
@@ -3,12 +3,19 @@ import re
 class ReleaseRequirements:
     input: str
     requirements: dict
+    updates: list
 
     def __init__(self,input: str):
         (rules_input, updates_input) = re.split (r'\n\s*\n', input)
-        self.rules = self.init_rules ( rules_input )
         
-        self.updates = re.split ( r'\n', updates_input)
+        self.init_rules ( rules_input )
+        self.init_updates ( updates_input )
+
+    def init_updates ( self, updates_input):
+        self.updates = []
+        for update in re.split ( r'\n', updates_input):
+            self.updates.append(list(map( int, update.split(','))))
+
 
     def init_rules ( self, rules_input): 
         self.requirements = {}
@@ -19,9 +26,3 @@ class ReleaseRequirements:
                 self.requirements[int(entry_page)] = []
             self.requirements[int(entry_page)].append ( int(required_page) )
 
-
-    def get_rules(self) -> list:
-        return self.rules
-    
-    def get_updates(self) -> list:
-        return self.updates
--- tests/test_day5.py
+++ tests/test_day5.py
@@ -2,17 +2,14 @@ import pytest
 
 from day5.ReleaseRequirements import ReleaseRequirements
 
-# @pytest.mark.parametrize('init_str, expected_width', [
-#     ('X', 1),
-#     ('XM\nAS', 2),
-#     ('XMA\nAXM', 3),
-# ])
-def test_requirements_init_updates() -> None:
+
+
+def test_requirements_init_updates ():
     testee = ReleaseRequirements('''10|12
                                  
 11,12
 12,7,10''')
-    assert len(testee.get_updates()) == 2
+    assert testee.updates == [[11,12], [12,7,10]]
 
 @pytest.mark.parametrize('init_str, expected_requirements', [
     ('''10|12
@@ -24,6 +21,6 @@ def test_requirements_init_updates() -> None:
                                  
 11,12''', {12: [11,10], 13: [10]}),
 ])
-def test_requirements_init_updates(init_str, expected_requirements) -> None:
+def test_requirements_init_rules(init_str, expected_requirements) -> None:
     testee = ReleaseRequirements(init_str)
     assert testee.requirements == expected_requirements
