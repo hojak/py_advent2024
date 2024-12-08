b844a29acad865bed2cbd773148b60688272dc66
Holger Janßen-Kroll <github@ho-jk.de>
2024-12-08 03:16:07 +0100

    feat: add main solver for day 7
    (part 1 working)

---
+++ src/day7/Solver.py
@@ -0,0 +1,11 @@
+from adv24_tools.problemSolver import problemSolver
+from day7.tools import add_reachable_lines
+
+
+class Solver(problemSolver):
+    def solve_part1(self):
+        print ("calibration result (sum of reachable numbers")
+        print (add_reachable_lines ( self.parsed_input))
+
+    def solve_part2(self):
+        print ( "not implemented, yet")
