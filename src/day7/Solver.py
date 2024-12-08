316e2df53548bbc0e7b48118fccd6a42e109e89b
Holger Janßen-Kroll <github@ho-jk.de>
2024-12-08 03:30:43 +0100

    feat: implent concatenation as possible operation
    (part 2 of day 7 solved)

--- src/day7/Solver.py
+++ src/day7/Solver.py
@@ -4,8 +4,9 @@ from day7.tools import add_reachable_lines
 
 class Solver(problemSolver):
     def solve_part1(self):
-        print ("calibration result (sum of reachable numbers")
+        print ("calibration result (sum of reachable numbers)")
         print (add_reachable_lines ( self.parsed_input))
 
     def solve_part2(self):
-        print ( "not implemented, yet")
+        print ("calibration result - also using concat operation")
+        print (add_reachable_lines ( self.parsed_input, True))
