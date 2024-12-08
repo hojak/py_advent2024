0d77f8ec3c3a4c551e4bdb936e7920dce2f87daa
Holger Janßen-Kroll <github@ho-jk.de>
2024-12-08 03:05:37 +0100

    feat: is_reachable working on sample input

--- src/day7/tools.py
+++ src/day7/tools.py
@@ -3,4 +3,7 @@ def is_reachable ( number: int, operants: list) -> bool :
     if ( len ( operants ) == 1):
         return ( number == operants[0])
     
-    return False
+    with_plus = [operants[0] + operants[1]]+ operants[2:]
+    with_multiply = [operants[0] * operants[1]]+ operants[2:]
+
+    return is_reachable(number, with_plus) or is_reachable ( number, with_multiply )
--- tests/test_day7.py
+++ tests/test_day7.py
@@ -5,6 +5,17 @@ from day7.tools import is_reachable
 @pytest.mark.parametrize('target, numbers, expected_result', [
     (10, [10], True),
     (10, [11], False),
+    (10, [2,5], True),
+    (9, [2,5], False),
+    (190, [10,19],True),
+    (3267, [81,40,27],True),
+    (83, [17,5],False),
+    (156, [15,6],False),
+    (7290, [6,8,6,15],False),
+    (161011, [16,10,13],False),
+    (192, [17,8,14],False),
+    (21037, [9,7,18,13],False),
+    (292, [11,6,16,20],True),
 ])
 def test_is_reachable(target, numbers, expected_result) -> None:
     assert is_reachable(target, numbers) == expected_result
