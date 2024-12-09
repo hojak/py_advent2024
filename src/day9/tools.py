7592d3110a0f16b4001b6cdf69ef5545299cb093
Holger Janßen-Kroll <github@ho-jk.de>
2024-12-10 00:01:17 +0100

    feat: compact works

--- src/day9/tools.py
+++ src/day9/tools.py
@@ -15,3 +15,27 @@ def expand ( input: str) -> list:
         next_is_block = not next_is_block
 
     return result
+
+
+def compact ( input: list) -> list :
+    result = input
+
+    next_free = 0
+    while ( input[next_free ] != '.' and next_free < len(input)):
+        next_free+=1
+
+    index_to_move = len(input)-1
+    while ( input[index_to_move] == '.' and index_to_move > 0):
+        index_to_move -= 1
+
+    while next_free < index_to_move:
+        result[next_free] = input[index_to_move]
+        result[index_to_move] = '.'
+
+        while ( input[next_free ] != '.' and next_free < len(input)):
+            next_free+=1
+        
+        while ( input[index_to_move] == '.' and index_to_move > 0):
+            index_to_move -= 1
+
+    return result
--- tests/test_day9.py
+++ tests/test_day9.py
@@ -1,6 +1,6 @@
 import pytest
 
-from day9.tools import expand
+from day9.tools import expand, compact
 
 @pytest.mark.parametrize('input, expected_expandation', [
     ('12345', [0,'.','.',1,1,1,'.','.','.','.',2,2,2,2,2]),
@@ -8,3 +8,12 @@ from day9.tools import expand
 ])
 def test_expand(input, expected_expandation) -> None:
     assert expand ( input ) == expected_expandation
+
+
+@pytest.mark.parametrize('input, expected_result', [
+    ([0,'.','.',1,1,1,'.','.','.','.',2,2,2,2,2], [0,2,2,1,1,1,2,2,2,'.','.','.','.','.','.']),
+    ([0,0,'.','.','.',1,1,1,'.','.','.',2,'.','.','.',3,3,3,'.',4,4,'.',5,5,5,5,'.',6,6,6,6,'.',7,7,7,'.',8,8,8,8,9,9],
+       [0,0,9,9,8,1,1,1,8,8,8,2,7,7,7,3,3,3,6,4,4,6,5,5,5,5,6,6,'.','.','.','.','.','.','.','.','.','.','.','.','.','.']  )
+])
+def test_compact(input, expected_result):
+    assert compact ( input ) == expected_result
