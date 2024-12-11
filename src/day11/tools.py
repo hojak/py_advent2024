cafae1a5baad405b2546034390e950d6f5116a92
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-12-11 17:20:47 +0100

    feat: rule 2, split stones by length

--- src/day11/tools.py
+++ src/day11/tools.py
@@ -3,8 +3,13 @@ import math
 def blink ( list_of_stones : list ) -> list :
     result = []
     for stone in list_of_stones:
+        stone_as_string = str(stone)
         if stone == 0:
             result.append(1)
+        elif len(stone_as_string) % 2 == 0:
+            half_length = math.floor(len(stone_as_string) / 2)
+            result.append ( int(stone_as_string[:half_length]))
+            result.append ( int(stone_as_string[half_length:]))
         else:
             result.append(stone)
 
--- tests/test_day11.py
+++ tests/test_day11.py
@@ -3,7 +3,9 @@ import pytest
 from day11.tools import blink
 
 @pytest.mark.parametrize('input, expected_list', [
-    ([0], [1] )
+    ([0], [1]),
+    ([11, 0], [1,1,1]),
+    ([1000, 0], [10, 0, 1]),
 ])
 def test_blink(input, expected_list):
     assert blink (input) == expected_list
