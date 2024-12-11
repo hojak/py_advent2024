7b1a2b0e41d34428f99e8a6d2ae1d14f09ce3582
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-12-11 17:26:57 +0100

    feat: blinking several times works

--- tests/test_day11.py
+++ tests/test_day11.py
@@ -10,3 +10,12 @@ from day11.tools import blink
 ])
 def test_blink(input, expected_list):
     assert blink (input) == expected_list
+
+@pytest.mark.parametrize('input, blinks, expected_output', [
+    ([125, 17], 6, [2097446912, 14168, 4048, 2, 0, 2, 4, 40, 48, 2024, 40, 48, 80, 96, 2, 8, 6, 7, 6, 0, 3, 2]),
+])
+def test_multiple_blinks(input, blinks, expected_output):
+    result = input
+    for i in range(blinks):
+        result = blink(result)
+    assert result == expected_output
