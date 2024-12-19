61331b11f636a744dc4d0212a1be2b3a89cf25ef
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-12-19 19:38:19 +0100

    feat: make the floor size an optional parameter

--- src/day14/BathroomSquat.py
+++ src/day14/BathroomSquat.py
@@ -4,41 +4,42 @@ import math
 
 class BathroomSquat:
     robots = []
-    field_size = Coordinates(11,7)
+    floor_size : Coordinates
 
-    def __init__(self, init_string):
+    def __init__(self, init_string, floor_size = Coordinates(11,7)):
         for robot_line in init_string.split ("\n"):
             self.robots.append ( Roboter.parse_line(robot_line))
+
+        self.floor_size = floor_size
 
 
     def march(self, number_of_seconds):
         for robot in self.robots:
-            robot.walk(number_of_seconds, self.field_size)
+            robot.walk(number_of_seconds, self.floor_size)
 
 
 
     def number_of_robots_in_quadrant(self, quadrant):
         quadrant_start_positions = [
             Coordinates(0,0),
-            Coordinates(self.field_size.x-math.floor (self.field_size.x / 2), 0),
-            Coordinates(0, self.field_size.y-math.floor (self.field_size.y / 2)),
-            Coordinates(self.field_size.x-math.floor (self.field_size.x / 2), self.field_size.y-math.floor (self.field_size.y / 2)),
+            Coordinates(self.floor_size.x-math.floor (self.floor_size.x / 2), 0),
+            Coordinates(0, self.floor_size.y-math.floor (self.floor_size.y / 2)),
+            Coordinates(self.floor_size.x-math.floor (self.floor_size.x / 2), self.floor_size.y-math.floor (self.floor_size.y / 2)),
         ]
         result = 0
 
         top_left = quadrant_start_positions[quadrant]
-        bottom_right = Coordinates(math.floor (self.field_size.x / 2)-1, math.floor (self.field_size.y / 2)-1).add ( top_left)
+        bottom_right = Coordinates(math.floor (self.floor_size.x / 2)-1, math.floor (self.floor_size.y / 2)-1).add ( top_left)
 
         for robot in self.robots:
             if robot.position.is_in_box ( top_left,bottom_right ):
-                print ( robot.position )
                 result += 1
 
         return result
     
-    def safty_factor ( self ):
+    def safety_factor ( self ):
         result = 1
-        for quadrant in range(3):
+        for quadrant in range(4):
             result *= self.number_of_robots_in_quadrant(quadrant)
 
         return result
--- tests/test_day14.py
+++ tests/test_day14.py
@@ -80,4 +80,4 @@ p=7,3 v=-1,2
 p=2,4 v=2,-3
 p=9,5 v=-3,-3''')
     testee.march (100)
-    assert testee.safty_factor() == 12
+    assert testee.safety_factor() == 12
