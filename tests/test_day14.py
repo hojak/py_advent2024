fbebf696157f80078a4845ec7d29ce2b5012f497
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-12-19 19:20:54 +0100

    test: add extended test for teleporting

--- tests/test_day14.py
+++ tests/test_day14.py
@@ -22,6 +22,22 @@ def test_roboter_step(init_line, expected_position):
     assert next_position == expected_position
 
 
+@pytest.mark.parametrize('init_line, steps, expected_position', [
+    ("p=2,4 v=2,-3", 0, Coordinates(2,4)),
+    ("p=2,4 v=2,-3", 1, Coordinates(4,1)),
+    ("p=2,4 v=2,-3", 2, Coordinates(6,5)),
+    ("p=2,4 v=2,-3", 3, Coordinates(8,2)),
+    ("p=2,4 v=2,-3", 4, Coordinates(10,6)),
+    ("p=2,4 v=2,-3", 5, Coordinates(1,3)),
+])
+def test_roboter_steps(init_line, steps, expected_position):
+    max_size = Coordinates(11,7)
+
+    testee = Roboter.parse_line(init_line)
+    testee.walk ( steps, max_size )
+
+    assert testee.position == expected_position
+
 def test_roboter_walk():
     max_size = Coordinates(150,150)
     testee = Roboter.parse_line("p=2,4 v=1,2")
