01acc03206f592def46a81996d7097ee875545db
Holger Janßen-Kroll <github@ho-jk.de>
2024-12-20 01:43:46 +0100

    feat: add solver for day 18

---
+++ src/day18/Solver.py
@@ -0,0 +1,18 @@
+from adv24Tools.problemSolver import problemSolver
+from adv24Tools.Coordinates import Coordinates
+from day18.MemoryGrid import *
+import sys
+
+class Solver(problemSolver):
+
+    def parse_input(self):
+        return parse_coordinates(self.input)
+    
+    def solve_part1(self):
+        testee = MemoryGrid(71,71)
+
+        for i in range(1024):
+            testee.mark_corruption(self.parsed_input[i])
+
+        print ("number of steps from (0/0) to (70/70)")
+        print ( testee.steps_to_exit(Coordinates(0,0), Coordinates(70,70)))
