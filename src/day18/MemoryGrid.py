31c2fa024ac6c526f5e9ee9ec856c5384ee4cc09
Holger Janßen-Kroll <github@ho-jk.de>
2024-12-20 02:04:05 +0100

    feat: find the blocking corruption

--- src/day18/MemoryGrid.py
+++ src/day18/MemoryGrid.py
@@ -1,5 +1,6 @@
 from adv24Tools.StringMap import StringMap
 from adv24Tools.Coordinates import Coordinates, directions
+import math
 
 
 def parse_coordinates ( input: str) -> [Coordinates] :
@@ -49,4 +50,29 @@ class MemoryGrid ( StringMap ):
                     queue.append( (next, steps_till_here+1))
                 
         return -1
+    
 
+
+    def find_blocking_coordinates (width, height, list_of_coordinates):
+        lower_end = 0
+        upper_end = len(list_of_coordinates)-1
+
+        while lower_end +1 != upper_end:
+            check = math.floor((upper_end+lower_end) / 2)
+            print ( str ( lower_end) + " - " + str(upper_end) + " -> " + str(check) )
+
+            grid = MemoryGrid.initialize_grid(width, height, list_of_coordinates[:check+1])
+
+            if ( grid.steps_to_exit(Coordinates(0,0), Coordinates(width-1, height-1)) >= 0):
+                lower_end = check
+            else:
+                upper_end = check
+
+        return list_of_coordinates[upper_end]
+    
+
+    def initialize_grid ( width, height, list_of_coordinates):
+        result = MemoryGrid(width, height)
+        for coordinates in list_of_coordinates:
+            result.mark_corruption(coordinates)
+        return result
