41ab8889eccadec224a6f5dc7a48559a829d693c
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-01-02 15:37:38 +0100

    feat: create arbitraty Keyholes

--- tests/test_day25.py
+++ tests/test_day25.py
@@ -1,8 +1,12 @@
 from day25.Keyhole import Keyhole
+import pytest
 
-
-def test_create_simple_keyhole ():
-    testee = Keyhole ( "#####\n.....\n.....\n.....\n.....\n.....\n.....")
-    expected_heights = [0,0,0,0,0]
-    for index in range(len(expected_heights)):
-        assert testee.get_height(index) == expected_heights[index]
+@pytest.mark.parametrize('init_string, expected_heights', [
+    ("#####\n.....\n.....\n.....\n.....\n.....\n.....", [0,0,0,0,0]),
+    ("#####\n#....\n.....\n.....\n.....\n.....\n.....", [1,0,0,0,0]),
+    ("#####\n#...#\n....#\n....#\n....#\n....#\n.....", [1,0,0,0,5]),
+])
+def test_create_simple_keyhole (init_string, expected_heights):
+    testee = Keyhole ( init_string)
+    for column in range(len(expected_heights)):
+        assert testee.get_height(column) == expected_heights[column]
