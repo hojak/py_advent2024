56a1d39f9ec4fda7d8da700b9e5d253756445926
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-01-02 15:31:05 +0100

    feat: introduce get_height

--- src/day25/Keyhole.py
+++ src/day25/Keyhole.py
@@ -2,3 +2,6 @@ class Keyhole:
 
     def __init__(self, definition: str):
         pass
+
+    def get_height(self, column: int) -> int:
+        return 0
--- tests/test_day25.py
+++ tests/test_day25.py
@@ -1,5 +1,8 @@
 from day25.Keyhole import Keyhole
 
-def test_create_keyhole ():
+
+def test_create_simple_keyhole ():
     testee = Keyhole ( "#####\n.....\n.....\n.....\n.....\n.....\n.....")
-    assert isinstance(testee, Keyhole)
+    expected_heights = [0,0,0,0,0]
+    for index in range(len(expected_heights)):
+        assert testee.get_height(index) == expected_heights[index]
