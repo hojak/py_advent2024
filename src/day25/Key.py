baf4d51976d0cc80715f60f7c53acdbbaa9f85f5
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-01-02 15:40:24 +0100

    feat: create keys

---
+++ src/day25/Key.py
@@ -0,0 +1,18 @@
+from adv24Tools.StringMap import StringMap
+from adv24Tools.Coordinates import Coordinates
+
+class Key:
+
+    def __init__(self, definition: str):
+        map = StringMap (definition)
+        self.heights = [0,0,0,0,0]
+
+        for column in range(len(self.heights)):
+            height = 0
+            while map.get_char_at(Coordinates(column,5-height)) == '#':
+                height += 1
+            self.heights[column] = height
+        
+
+    def get_height(self, column: int) -> int:
+        return self.heights[column]
--- tests/test_day25.py
+++ tests/test_day25.py
@@ -1,4 +1,5 @@
 from day25.Keyhole import Keyhole
+from day25.Key import Key
 import pytest
 
 @pytest.mark.parametrize('init_string, expected_heights', [
@@ -6,7 +7,17 @@ import pytest
     ("#####\n#....\n.....\n.....\n.....\n.....\n.....", [1,0,0,0,0]),
     ("#####\n#...#\n....#\n....#\n....#\n....#\n.....", [1,0,0,0,5]),
 ])
-def test_create_simple_keyhole (init_string, expected_heights):
+def test_create_keyholes (init_string, expected_heights):
     testee = Keyhole ( init_string)
+    for column in range(len(expected_heights)):
+        assert testee.get_height(column) == expected_heights[column]
+
+
+@pytest.mark.parametrize('init_string, expected_heights', [
+    (".....\n.....\n.....\n.....\n.....\n.....\n#####", [0,0,0,0,0]),
+    (".....\n....#\n....#\n....#\n....#\n#...#\n#####", [1,0,0,0,5]),
+])
+def test_create_keys (init_string, expected_heights):
+    testee = Key ( init_string)
     for column in range(len(expected_heights)):
         assert testee.get_height(column) == expected_heights[column]
