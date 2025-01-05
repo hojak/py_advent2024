85ae19a8e383a519a83e59db1c411487d7a061c2
Holger Janßen-Kroll <github@ho-jk.de>
2025-01-05 12:05:05 +0100

    feat: introduce directionpad robot

--- src/day21/NumpadRobot.py
+++ src/day21/NumpadRobot.py
@@ -17,6 +17,13 @@ class Robot(StringMap):
     def current_position(self):
         return self.position
     
+
+    
+
+class NumpadRobot (Robot):
+    def __init__(self):
+        super().__init__("789\n456\n123\n 0A", Coordinates(2,3))
+
     def go_to_key(self, key):
         target = self.get_coordinates_for(key)
         path = ''
@@ -36,11 +43,30 @@ class Robot(StringMap):
         self.position = target
 
         return path
-    
 
-class NumpadRobot (Robot):
-
-    def __init__(self):
-        super().__init__("789\n456\n123\n 0A", Coordinates(2,3))
         
+class DirectionpadRobot (Robot):
+    def __init__(self):
+        super().__init__(" ^A\n<v>", Coordinates(2,0))
+
+    def go_to_key(self, key):
+        target = self.get_coordinates_for(key)
+        path = ''
+
+        if ( target.y > self.current_position().y):
+            path += "v" * (target.y - self.current_position().y)
+
+        if ( target.x > self.current_position().x):
+            path += ">" * (target.x - self.current_position().x)
+
+        if ( target.y < self.current_position().y):
+            path += "^" * (self.current_position().y - target.y)
+
+        if ( target.x < self.current_position().x):
+            path += "<" * (self.current_position().x - target.x)
+
+
+        self.position = target
+
+        return path        
     
--- tests/test_day21.py
+++ tests/test_day21.py
@@ -1,4 +1,4 @@
-from day21.NumpadRobot import NumpadRobot
+from day21.NumpadRobot import NumpadRobot, DirectionpadRobot
 from adv24Tools.Coordinates import Coordinates
 
 import pytest
@@ -26,6 +26,20 @@ def test_robot_get_coordinates_of_key():
 ])
 def test_numpad_robot_path_between_given_keys(start_key, target_key, expected_path):
     testee = NumpadRobot()
+    testee.position = testee.get_coordinates_for(start_key)
+    assert testee.go_to_key(target_key) == expected_path
+    assert testee.current_position() == testee.get_coordinates_for(target_key)
+
+
+
+@pytest.mark.parametrize('start_key, target_key, expected_path', [
+    ("A", "A", ""),
+    ("^", "^", ""),
+    ("<", "A", ">>^"),
+    ("A", "<", "v<<"),
+])
+def test_directionpad_robot_path_between_given_keys(start_key, target_key, expected_path):
+    testee = DirectionpadRobot()
     testee.position = testee.get_coordinates_for(start_key)
     assert testee.go_to_key(target_key) == expected_path
     assert testee.current_position() == testee.get_coordinates_for(target_key)
