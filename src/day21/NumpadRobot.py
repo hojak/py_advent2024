e22064848c712fd271e371c0a7e4b1f689f52caf
Holger Janßen-Kroll <github@ho-jk.de>
2025-01-05 11:59:43 +0100

    refactor: extracted Robot super class

--- src/day21/NumpadRobot.py
+++ src/day21/NumpadRobot.py
@@ -1,20 +1,21 @@
 from adv24Tools.Coordinates import Coordinates
 from adv24Tools.StringMap import StringMap
 
-class NumpadRobot (StringMap):
 
-    def __init__(self):
-        super().__init__("789\n456\n123\n 0A")
-        self.position = Coordinates(2,3)
-        
-    def current_position(self):
-        return self.position
+class Robot(StringMap):
+
+    def __init__(self, map, start_position):
+        super().__init__(map)
+        self.position = start_position
+
+    def get_coordinates_for(self, key):
+        return self.coordinates_for_index(self.content.index(key))
     
     def current_key(self):
         return self.get_char_at(self.position)
-    
-    def get_coordinates_for(self, key):
-        return self.coordinates_for_index(self.content.index(key))
+
+    def current_position(self):
+        return self.position
     
     def go_to_key(self, key):
         target = self.get_coordinates_for(key)
@@ -36,4 +37,10 @@ class NumpadRobot (StringMap):
 
         return path
     
+
+class NumpadRobot (Robot):
+
+    def __init__(self):
+        super().__init__("789\n456\n123\n 0A", Coordinates(2,3))
+        
     
