cad8ec634cb989f31b7ee315fb97092ccb8bb0cb
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-01-17 14:39:19 +0100

    refactor: extract magic constants

--- src/day15/Warehouse.py
+++ src/day15/Warehouse.py
@@ -1,6 +1,8 @@
 from adv24Tools.StringMap import StringMap
 from adv24Tools.Coordinates import Coordinates
 class Warehouse (StringMap):
+    free_space = '.'
+    robot = '@'
 
     def __init__(self, map):
         super().__init__(map)
@@ -9,16 +11,16 @@ class Warehouse (StringMap):
         return self.coordinates_for_index(self.content.index(key))
     
     def get_robot_position(self):
-        return self.get_coordinates_for('@')
+        return self.get_coordinates_for(Warehouse.robot)
     
     def move(self, direction):
         current_position = self.get_robot_position()
 
         next_position = current_position.add(Warehouse.vector_for_direction(direction))        
         
-        if ( self.get_char_at(next_position) == '.'):
-            self.set_char_at(current_position, '.')
-            self.set_char_at(next_position, '@')
+        if ( self.get_char_at(next_position) == Warehouse.free_space):
+            self.set_char_at(current_position, Warehouse.free_space)
+            self.set_char_at(next_position, Warehouse.robot)
 
     def vector_for_direction(direction):
         vectors = {
