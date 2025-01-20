9372800ae4a3efc12b8650fd51259b13cf7992bf
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-01-20 13:07:48 +0100

    feat: maze initzialized with start and end position

--- src/day16/ReindeerMaze.py
+++ src/day16/ReindeerMaze.py
@@ -4,8 +4,10 @@ from adv24Tools.Coordinates import Coordinates
 class ReindeerMaze(StringMap):
 
     start_position_char = "S"
+    end_position_char = "E"
 
     def __init__(self, map):
         super().__init__(map)
 
         self.start_position = self.get_coordinates_for(ReindeerMaze.start_position_char)
+        self.end_position = self.get_coordinates_for(ReindeerMaze.end_position_char)
--- tests/test_day16.py
+++ tests/test_day16.py
@@ -4,9 +4,17 @@ import pytest
 
 
 @pytest.mark.parametrize('map_description, expected_start_position', [
-    ('###\n#S#\n###', Coordinates(1,1)), 
-    ('####\n#..#\n#.S#\n####', Coordinates(2,2)), 
+    ('####\n#SE#\n####', Coordinates(1,1)), 
+    ('####\n#E.#\n#.S#\n####', Coordinates(2,2)), 
 ])
 def test_maze_with_start(map_description, expected_start_position):
     testee = ReindeerMaze(map_description)
     assert testee.start_position == expected_start_position
+
+@pytest.mark.parametrize('map_description, expected_end_position', [
+    ('####\n#ES#\n####', Coordinates(1,1)), 
+    ('####\n#S.#\n#.E#\n####', Coordinates(2,2)), 
+])
+def test_maze_with_end(map_description, expected_end_position):
+    testee = ReindeerMaze(map_description)
+    assert testee.end_position == expected_end_position
