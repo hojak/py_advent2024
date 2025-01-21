22876e9fcda8db12bdfab76a62a9902fb2073a67
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-01-21 15:12:32 +0100

    feat: implement solver for part 2
    hint: performance could be better

--- src/day16/Solver.py
+++ src/day16/Solver.py
@@ -5,7 +5,15 @@ class Solver (problemSolver):
 
     def parse_input(self):
         return ReindeerMaze(self.input)
-    
+
+    ## todo:
+    # algorithm itself works, could be faster, probably runtime could benefit from 
+    # simple optimizations as queue handling, list merging etc.
+
     def solve_part1(self):
         print("Lowest possible score for this map:")
         print ( self.parsed_input.lowest_score_for_path_to_finish() )
+
+    def solve_part2(self):
+        print("Number of interesting fields")
+        print ( self.parsed_input.get_number_of_interesting_path_coordinates())
