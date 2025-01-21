d2f92ce635d2b6a92a48188f05cf593b9f52892f
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-01-21 17:28:06 +0100

    feat: implement combo operant

--- src/day17/Computer.py
+++ src/day17/Computer.py
@@ -8,8 +8,24 @@ class Computer:
         self.registerA = get_value_for_register(registers[0])
         self.registerB = get_value_for_register(registers[1])
         self.registerC = get_value_for_register(registers[2])
+
+        (unimportant, program) = program.split(": ")
+        self.program = list( map( lambda str: int(str), program.strip().split(',')))
+
+    def combo(self, operant):
+        match operant:
+            case 0|1|2|3:
+                return operant
+            case 4:
+                return self.registerA
+            case 5:
+                return self.registerB
+            case 6:
+                return self.registerC
+
         
 
 def get_value_for_register(str):
     (name, value) = str.strip().split(":")
     return int(value)
+
--- tests/test_day17.py
+++ tests/test_day17.py
@@ -10,3 +10,27 @@ Program: 0,1,5,4,3,0''')
     assert testee.registerA == 729
     assert testee.registerB == 0
     assert testee.registerC == 0
+    
+def test_initialize_computer_program():
+
+    testee = Computer('''Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0''')
+    assert testee.program == [0,1,5,4,3,0]
+
+
+def test_combo():
+    testee = Computer('''Register A: 10
+Register B: 20
+Register C: 30
+
+Program: 0''')
+    assert testee.combo(0) == 0
+    assert testee.combo(1) == 1
+    assert testee.combo(2) == 2
+    assert testee.combo(3) == 3
+    assert testee.combo(4) == 10
+    assert testee.combo(5) == 20
+    assert testee.combo(6) == 30
