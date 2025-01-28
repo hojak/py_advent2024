ed73514f0cf0932f9dc1951b9a75caba059ac536
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-01-28 11:58:55 +0100

    feat: and gate returns 0 for diffentent inputs

--- src/day24/gates.py
+++ src/day24/gates.py
@@ -2,3 +2,16 @@ class ConstantGate:
     def __init__(self, name, value):
         self.name = name
         self.output = value
+
+    def get_output(self):
+        return self.output
+
+
+class AndGate:
+    def __init__(self, name, input1, input2):
+        self.name = name
+        self.input1 = input1
+        self.input2 = input2
+
+    def get_output(self):
+        return self.input1.get_output() and self.input2.get_output()
--- tests/test_day24.py
+++ tests/test_day24.py
@@ -3,4 +3,12 @@ from day24.gates import *
 def test_constant_gate():
     testee = ConstantGate("x01", 1)
     assert testee.name == "x01"
-    assert testee.output == 1
+    assert testee.get_output() == 1
+
+
+def test_and_gate():
+    input1 = ConstantGate("x0", 0)    
+    input2 = ConstantGate("x1", 1)
+
+    testee = AndGate("and", input1, input2)
+    assert testee.get_output() == 0
